68c38394f20387e518dea60b4640005e5800a14e
GonzalezCortesSergio <gonzalez.coser24@triana.salesianos.edu>
2024-11-18 22:04:20 +0100

    First commit

---
+++ delight-nook-back/src/main/java/com/salesianostriana/dam/delight_nook_back/DelightNookBackApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.delight_nook_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelightNookBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelightNookBackApplication.class, args);
+	}
+
+}
